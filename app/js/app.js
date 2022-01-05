717dbbbeeef3defcfeacd2833f77f730aee01674
TToushirou <46146484+TToushirou@users.noreply.github.com>
2022-01-05 07:35:27 +0000

    test

---
+++ app/js/app.js
@@ -0,0 +1,94 @@
+const index = document.querySelector('.form');
+let firstname = document.getElementById('firstname')
+let lastname = document.getElementById('lastname')
+let email = document.getElementById('email');
+let message = document.getElementById('message');
+
+
+
+index.addEventListener('submit', (e)=>{
+	e.preventDefault();
+	
+	let formData = {
+		firstname: firstname.value,
+		lastname: lastname.value,
+		email: email.value,
+		message: message.value
+	}
+
+	let xhr = new XMLHttpRequest();
+	xhr.open('POST', '/');
+	xhr.setRequestHeader('content-type', 'application/json');
+	xhr.onload = function() {
+		console.log(xhr.responseText);
+		if(xhr.responseText == 'success'){
+			firstname.value = '';
+			lastname.value = '';
+			email.value = '';
+			message.value = '';
+		}else {
+			alert('something went wrong')
+		}
+	}
+
+	xhr.send(JSON.stringify(formData));
+
+
+	checkInputs();
+
+});
+
+
+function checkInputs(){
+	const firstnameValue = firstname.value.trim();
+	const lastnameValue = lastname.value.trim();
+	const emailValue = email.value.trim();
+	const messageValue = message.value.trim();
+
+	if(firstnameValue === '') {
+		setErrorFor(firstname, ' First name cannot be blank');
+	} else {
+		setSuccessFor(firstname);
+	}
+
+	if(lastnameValue === '') {
+		setErrorFor(lastname, ' Last name cannot be blank');
+	} else {
+		setSuccessFor(lastname);
+	}
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+		
+	}
+	
+	if(messageValue === '') {
+		setErrorFor(message, 'Message cannot be blank');
+	} else if (messageValue.length < 7) {
+		setErrorFor(message, 'Message Less than 7 characters');
+	} else {
+		setSuccessFor(message);
+	}
+};
+
+
+function setErrorFor(input, message) {
+	const formc = input.parentElement;
+	const small = formc.querySelector('small');
+	formc.className = 'formc error';
+	small.innerText = message;
+};
+
+function setSuccessFor(input) {
+	const formc = input.parentElement;
+	formc.className = 'formc success';
+};
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+};
+
---
+++ server.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const { info } = require('autoprefixer');
+const { text } = require('express');
+const express = require('express');
+const app = express();
+
+
+
+const PORT = process.env.PORT || 5000;
+
+/*
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+});
+*/
+
+const nodemailer = require("nodemailer");
+
+
+//middleware 
+app.use(express.static('app'));
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+	res.sendFile(__dirname + '/app/index.html')
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body)
+
+
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_ID,
+            pass: process.env.PASSWORD
+        }
+    });
+
+    const mailOptions = {
+        from: req.body.email,
+        to: process.env.EMAIL_ID,
+        subject: `Message from${req.body.email}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }else{
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running onport ${PORT}`)
+})
